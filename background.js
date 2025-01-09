7531a3cfc3e33f57f3a9d10d012c06826deb9330
Choucheneeee <chouchene.amine.etud@gmail.com>
2025-01-09 21:14:06 +0100

    modify the message for success

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 // background.js (Chrome Extension background script)
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.type === "someType") {
-      sendResponse({ status: "success", data: "Your Data" });
+      sendResponse({ status: "success ! ", data: "Your Data" });
     }
     return true; 
   });
